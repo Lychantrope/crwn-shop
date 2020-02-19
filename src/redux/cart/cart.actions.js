f7676031f4577c9ee1997e6734650b97be4afea4
Lora Doychinova <lora.doychinova@gmail.com>
2020-02-19 15:32:47 +0200

    adding cartIcon and cartDropdoen componenets, implementing cart reducer

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,5 @@
+import CartActionTypes from './cart.types';
+//The Action is a FUNCTION (not an object) that always has a type and sometimes has a payload. 
+export const toggleCartHidden = () => ({
+    type: CartActionTypes.TOGGLE_CART_HIDDEN
+})
